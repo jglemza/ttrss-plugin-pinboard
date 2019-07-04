c2fce51c8e4f18acf6b2967d049593a491c0bbc4
Joshua Glemza <josh@nugen.me>
2019-07-04 11:37:35 -0400

    Rebase

--- init.php
+++ init.php
@@ -1,19 +1,17 @@
 <?php
 class Pinboard extends Plugin {
-	private $link;
 	private $host;
 
 	function init($host) {
-		$this->link = $host->get_link();
 		$this->host = $host;
 
 		$host->add_hook($host::HOOK_ARTICLE_BUTTON, $this);
 	}
 
 	function about() {
-		return array(1.0,
-			"Save article on pinboard",
-			"dlc");
+		return array(1.2,
+			"Share article on Pinboard",
+			"m.s");
 	}
 
 	function get_js() {
@@ -23,18 +21,18 @@ class Pinboard extends Plugin {
 	function hook_article_button($line) {
 		$article_id = $line["id"];
 
-		$rv = "<img src=\"plugins.local/pinboard/pinboard.png\"
-			class='tagsPic' style=\"cursor : pointer\"
-			onclick=\"pinboardSaveArticle($article_id)\"
-			title='".__('Save on Piboard')."'>";
+		$rv = "<img src=\"plugins.local/pinboard/pinboard.png\" 
+			class='tagsPic' style=\"cursor : pointer\" 
+			onclick=\"shareArticleToPinboard($article_id)\" 
+			title='".__('Share on Pinboard')."'>";
 
 		return $rv;
 	}
 
 	function getInfo() {
-		$id = db_escape_string($this->link, $_REQUEST['id']);
+		$id = db_escape_string($_REQUEST['id']);
 
-		$result = db_query($this->link, "SELECT title, link
+		$result = db_query("SELECT title, link
 				FROM ttrss_entries, ttrss_user_entries
 				WHERE id = '$id' AND ref_id = id AND owner_uid = " .$_SESSION['uid']);
 
@@ -48,5 +46,8 @@ class Pinboard extends Plugin {
 				"id" => $id));
 	}
 
+	function api_version() {
+		return 2;
+	}
 
 }
