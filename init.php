922febff9e45a01702acfbaa8e0ce0b90c2ec12f
Joshua Glemza <josh@nugen.me>
2019-07-04 12:21:48 -0400

    Initial PDO implementation

--- init.php
+++ init.php
@@ -43,10 +43,10 @@ class Pinboard extends Plugin {
                 //                FROM ttrss_entries, ttrss_user_entries
                 //                WHERE id = '$id' AND ref_id = id AND owner_uid = " .$_SESSION['uid']);
 
-                if (db_num_rows($result) != 0) {
-                        $title = truncate_string(strip_tags(db_fetch_result($result, 0, 'title')),
+                if ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                        $title = truncate_string(strip_tags($row['title']),
                                 100, '...');
-                        $article_link = db_fetch_result($result, 0, 'link');
+                        $article_link = $row['link'];
                 }
 
                 print json_encode(array("title" => $title, "link" => $article_link,
