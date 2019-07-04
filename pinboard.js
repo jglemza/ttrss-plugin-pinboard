2f305fa1100249391544ce282c0822b9305137e4
Joshua Glemza <josh@nugen.me>
2019-07-04 11:39:13 -0400

    Rebase JS

--- pinboard.js
+++ pinboard.js
@@ -1,30 +1,28 @@
-function pinboardSaveArticle(id) {
-    try {
-        var query = "?op=pluginhandler&plugin=pinboard&method=getInfo&id=" + id;
+function shareArticleToPinboard(id) {
+	try {
+		var query = "?op=pluginhandler&plugin=pinboard&method=getInfo&id=" + param_escape(id);
 
-        console.log(query);
+		console.log(query);
 
-        var d = new Date();
-        var ts = d.getTime();
+		var d = new Date();
+      var ts = d.getTime();
 
-        var w = window.open('backend.php?op=backend&method=loading', 'ttrss_pinboard',
-            "status=0,toolbar=0,location=0,width=500,height=400,scrollbars=1,menubar=0");
+		var w = window.open('backend.php?op=backend&method=loading', 'ttrss_tweet',
+			"status=0,toolbar=0,location=0,width=650,height=350,scrollbars=1,menubar=0");
 
-        new Ajax.Request("backend.php", {
-            parameters: query,
-            onComplete: function(transport) {
-                var ti = JSON.parse(transport.responseText);
+		new Ajax.Request("backend.php",	{
+			parameters: query,
+			onComplete: function(transport) {
+				var ti = JSON.parse(transport.responseText);
 
-                var share_url = "https://pinboard.in/add/?" + 
-                    "&title=" + ti.title +
-                    "&url=" + ti.link;
+				var share_url = "http://pinboard.in/add?url=" + param_escape(ti.link) + "&title=" + param_escape(ti.title);
 
-                w.location.href = share_url;
+				w.location.href = share_url;
 
-            } });
+			} });
 
 
-    } catch (e) {
-        exception_error("pinboardSaveArticle", e);
-    }
+	} catch (e) {
+		exception_error("tweetArticle", e);
+	}
 }
